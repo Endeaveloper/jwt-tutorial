11fa40925bf082b94f3f3bd1aab6518b0e4869f2
Endeaveloper <dolp17@gmail.com>
2021-05-04 16:58:02 +0900

    NPE

---
+++ SpringTest_jdk11/test-spring/src/main/java/cleanCode/NullPE.java
@@ -0,0 +1,26 @@
+package cleanCode;
+
+public class NullPE {
+
+/*
+        상관없음
+        if (ucUser.getName != null) {
+        }
+
+        상관없음
+        if (null  !=  ucUser.getName) {
+        }
+        상관없음
+        if("".equals(value.getStringName()){
+        //do something
+        }
+
+
+        => null.equals 널에 메소드를 쓸 수 없어서 NPE
+        if (null.equals(value.getString)) {
+
+        }
+
+*/
+
+}
